c3b822b0b26f8f8983ce310abd26862b6ae2021f
Eran Rotem <eranrotem123@gmail.com>
2023-03-16 13:57:10 -0500

    init commit

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        private HttpClient _client;
+
+        public RonVSKanyeAPI(HttpClient client)
+        { _client = client; }
+
+        public string KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwansonQuote()
+        {
+            var ronSURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronSResponse = _client.GetStringAsync(ronSURL).Result;
+
+            var ronSQuote = JArray.Parse(ronSResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronSQuote;
+        }
     }
 }
