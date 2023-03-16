8e24702c144b27be26a885a4ec86f2a1a8145b34
Eran Rotem <eranrotem123@gmail.com>
2023-03-16 14:57:12 -0500

    init commit

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq; 
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        private HttpClient _client;
+        public string key = "3286af5bd843f275d8cec866606a7ffd";
+        public OpenWeatherMapAPI(HttpClient client) 
+        {
+            _client = client;
+        }
+
+        public string GetWeatherDetails(string cityName)
+        {
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={key}";
+
+            var weatherRes = _client.GetStringAsync(weatherURL).Result;
+
+            var weather = JObject.Parse(weatherRes).GetValue("main").ToString();
+
+            return $"The current weather in {cityName} is as follows: {weather}";
+        }
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -6,12 +6,15 @@
         {
             var client = new HttpClient();
             var quote = new RonVSKanyeAPI(client);
+            var weather = new OpenWeatherMapAPI(client);
 
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"Kanye: \"{quote.KanyeQuote()}\"");
                 Console.WriteLine($"Ron Swanson: {quote.RonSwansonQuote()}");
             }
+
+            Console.WriteLine(weather.GetWeatherDetails("Chicago"));
         }
     }
 }
